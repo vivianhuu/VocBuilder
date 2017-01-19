ac6354359819a18e9d1b23590fb2cb856185c28b
Vivian <vivianhuzefan@gmail.com>
2017-01-19 17:40:15 +0800

    the main definition of the app

--- client/js/app.js
+++ client/js/app.js
@@ -92,6 +92,27 @@ VocBuilder.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', 
 		$event.stopPropagation();
 	};
 })
-.run(['$state', function ($state){
+.run(['$state', '$rootScope', function ($state, $rootScope){
 	$state.go('login');
+	$rootScope.$on('$stateChangeStart',function(event, toState, toParams, fromState, fromParams){
+  		console.log('$stateChangeStart to '+toState.to+'- fired when the transition begins. toState,toParams : \n',toState, toParams);
+	});
+
+	$rootScope.$on('$stateChangeError',function(event, toState, toParams, fromState, fromParams){
+  		console.log('$stateChangeError - fired when an error occurs during transition.');
+	  	console.log(arguments);
+	});
+
+	$rootScope.$on('$stateChangeSuccess',function(event, toState, toParams, fromState, fromParams){
+	  	console.log('$stateChangeSuccess to '+toState.name+'- fired once the state transition is complete.');
+	});
+
+	$rootScope.$on('$viewContentLoaded',function(event){
+  	  	console.log('$viewContentLoaded - fired after dom rendered',event);
+	});
+
+	$rootScope.$on('$stateNotFound',function(event, unfoundState, fromState, fromParams){
+  		console.log('$stateNotFound '+unfoundState.to+'  - fired when a state cannot be found by its name.');
+  		console.log(unfoundState, fromState, fromParams);
+	});
 }]);
